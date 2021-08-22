48bee919f3dcbe0f3b43da9eac94f56093c2beb3
suraj1124 <kumarsuraj64@yahoo.in>
2021-08-22 09:06:08 -0700

    changed .java file

--- javaprog.java
+++ javaprog.java
@@ -1,6 +1,7 @@
 class javaprog {
-public	static void main (string[] args){
-system.out.printin{"Hello there this is suraj java project"};
+public	static void main(string[] args){
+system.out.printin("Hello there this is suraj java project");
+
 
 
 }
